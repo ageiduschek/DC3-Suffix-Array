6f7af97006609b17600bc1d5dd4cb4c61edd864e
Anna Geiduschek <annageid@stanford.edu>
2014-05-28 16:37:24 -0700

    Started fleshing some methods for Generalized Suffix Arrays. Going to start implementing DC3 next.

--- src/SuffixArray.hs
+++ src/SuffixArray.hs
@@ -7,7 +7,8 @@ type StrNum = Index
 
 type LCPInfo = [Length]
 
-data SuffixRankings = SingleSuffixRankings [Index] | GeneralizedSuffixRankings [(StrNum, Index)]
+data SuffixRankings = SuffixRankings [Index] 
+data GeneralizedSuffixRankings = GeneralizedSuffixRankings [(StrNum, Index)]
 
 data StrChar = ActualChar Char | PseudoEOF Index
 
@@ -23,44 +24,74 @@ instance Ord StrChar where
     _ `compare` _ = GT
 
 -- A suffix array for a single string
-data SuffixArray = SuffixArray{ inputStr :: [StrChar]  
+data SuffixArray = SuffixArrayConstructor { inputStr :: [StrChar]  
                      , orderedSuffixes :: SuffixRankings  
-                     , lcp :: LCPInfo 
+                     , lcp :: LCPInfo
                      , lcpRMQ :: FischerHeun  
                      } 
 
 -- A suffix array for many strings
-data GeneralizedSuffixArray = GeneralizedSuffixArray {} -- TODO: Implement
+data GeneralizedSuffixArray = GeneralizedSuffixArrayConstructor {  inputStrs :: [[StrChar]] 
+                                                        , genOrderedSuffix :: GeneralizedSuffixRankings
+                                                        , numInputStrs :: Int 
+                                                        , strIndexToOverallIndex :: (StrNum, Index) -> Index 
+                                                        , genLcp :: LCPInfo 
+                                                        , genLcpRMQ :: FischerHeun  
+                                                        } 
+
+initialAlphabetSize :: Int
+initialAlphabetSize = 256
 
 createSuffixArray :: String -> SuffixArray
-createSuffixArray str = SuffixArray {inputStr = str', orderedSuffixes = arr, lcp = lcp, lcpRMQ = lcpRMQ}
+createSuffixArray str = SuffixArrayConstructor {inputStr = str', orderedSuffixes = arr, lcp = lcp, lcpRMQ = lcpRMQ}
     where 
-        str' = (appendEOF str)
-        arr =  SingleSuffixRankings $ strToSuffixArray str' 256
-        lcp = generateLCPInfo arr
+        str' = appendEOF str 0
+        arr =  SuffixRankings $ strToSuffixArray str' initialAlphabetSize
+        lcp = lCPInfo arr
         lcpRMQ = fischerHeunRMQ lcp
 
 
 -- Creates a generalized suffix array out of a list of strings
 createGeneralizedSuffixArray :: [String] -> GeneralizedSuffixArray
-createGeneralizedSuffixArray strs = undefined
-    -- toGeneralized strs $ strToSuffixArray $ concatenateInputStrs strs
+createGeneralizedSuffixArray strs = GeneralizedSuffixArrayConstructor {  inputStrs = inputStrs 
+                                                                        , genOrderedSuffix = arr 
+                                                                        , numInputStrs = length inputStrs
+                                                                        , strIndexToOverallIndex = toOverallAddr  
+                                                                        , genLcp = lcp 
+                                                                        , genLcpRMQ = lcpRMQ  
+                                                                    }
+     where 
+        inputStrs = zipWith (appendEOF) strs [0..]
+        (toIndividualStrAddr, toOverallAddr) = getToFromGeneralMaps inputStrs
+        arr =  GeneralizedSuffixRankings $ map toIndividualStrAddr $ strToSuffixArray (concat inputStrs) initialAlphabetSize
+        lcp = genLCPInfo arr
+        lcpRMQ = fischerHeunRMQ lcp
 
-concatenateInputStrs :: [String] -> [StrChar]
-concatenateInputStrs strs = undefined
 
+-- This performs the DC3 Algorithm
 strToSuffixArray :: [StrChar] -> Int -> [Index]
 strToSuffixArray str alphabetSize = undefined
 
-appendEOF :: String -> [StrChar]
-appendEOF str = undefined
+appendEOF :: String -> Index -> [StrChar]
+appendEOF [] index = (PseudoEOF index):[]
+appendEOF (x:xs) index = (ActualChar x):(appendEOF xs index)
 
-toGeneralized :: [String] -> [Index] -> [(StrNum, Index)]
-toGeneralized strs sa = undefined
+getToFromGeneralMaps :: [[StrChar]] -> (Index -> (StrNum, Index), (StrNum, Index) -> Index)
+getToFromGeneralMaps strs = (toIndividualStrAddr, toOverallAddr)
+    where 
+        strsWithIndices = zip [0..] strs --[(0, "foo$"), (1, "bar$"), (2, "a$")]
+        strsOfIndices = map (\(i, str) -> replicate (length str) i) strsWithIndices -- [[0, 0, 0, 0], [1, 1, 1, 1], [2, 2]]
+        individualStrAddresses = concat $ map (\x -> zip x [0..]) strsOfIndices -- [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 2), ...]
+        toIndividualStrAddr i =  individualStrAddresses !! i
+        toOverallAddr (strNum, i) = i + (map length strsOfIndices) !! strNum
 
-generateLCPInfo :: SuffixRankings -> LCPInfo
-generateLCPInfo orderedSuffixes = undefined
 
+-- The following two functions will need to implement the algorithm in http://www.cs.iastate.edu/~cs548/references/linear_lcp.pdf
+lCPInfo :: SuffixRankings -> LCPInfo
+lCPInfo indices = undefined
+
+genLCPInfo :: GeneralizedSuffixRankings -> LCPInfo
+genLCPInfo indices = undefined
 
 -- Takes a string and a user provided eof character that doesn't appear anywhere in the string
 toBurrowsWheeler :: String -> Char -> String
@@ -72,4 +103,8 @@ fromBurrowsWheeler str eof = undefined
 
 -- Longest common extension. Array of indices must match number of strings in GeneralizedSuffixArray
 lce :: GeneralizedSuffixArray -> [Index] -> Int
-lce sa indices = undefined
+lce sa indices = if length indices /= numInputStrs sa 
+                    then error "must provide same number of indices as there are in the GeneralizedSuffixArray" 
+                    else (genLcpRMQ sa) (minimum addresses) (maximum addresses)
+        where 
+            addresses = map (strIndexToOverallIndex sa) (zip [0..] indices)
